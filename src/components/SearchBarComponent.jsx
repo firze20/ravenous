a31c1ad96d330719984ed511c66e12fb1c941161
FabioPin <fajos.pinheiro@hotmail.com>
2023-07-10 18:19:30 +0100

    space around

--- src/components/SearchBarComponent.jsx
+++ src/components/SearchBarComponent.jsx
@@ -1,10 +1,14 @@
 import React from "react";
 
 export const SearchBarComponent = () => {
-    return(
-        <div className="search">
-            <h1>Revenous</h1>
-            <button>Let's Go</button>
-        </div>
-    )
-}
+  return (
+    <div className="search">
+      <div className="subHeader">
+        <h2>Best Match</h2>
+        <h2>Highest Rated</h2>
+        <h2>Most Reviewed</h2>
+      </div>
+      <button className="buttonSearch">Let's Go</button>
+    </div>
+  );
+};
