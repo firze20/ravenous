d99cb616ba2a7146f6c5c0e81898fd5b97b5683e
FabioPin <fajos.pinheiro@hotmail.com>
2023-07-11 00:35:32 +0100

    composed code

--- src/components/SearchBarComponent.jsx
+++ src/components/SearchBarComponent.jsx
@@ -1,24 +1,35 @@
 import React, { useState } from "react";
 
 export const SearchBarComponent = () => {
+  const [restaurant, setRestauraunt] = useState("");
+  const [location, setLocation] = useState("");
 
-    const [restaurant, setRestauraunt] = useState("");
-    const [location, setLocation] = useState("");
-
- const onSubClick = () => {
-    alert("Clicou! 🥴")
- }
+  const onSubClick = () => {
+    alert("Clicou! 🥴");
+  };
 
   return (
     <div className="search">
       <div className="subHeader">
-        <button className="headButton" onClick={onSubClick}>Best Match</button>
-        <button className="headButton" onClick={onSubClick}>Highest Rated</button>
-        <button className="headButton" onClick={onSubClick}>Most Reviewed</button>
+        <button className="headButton" onClick={onSubClick}>
+          Best Match
+        </button>
+        <button className="headButton" onClick={onSubClick}>
+          Highest Rated
+        </button>
+        <button className="headButton" onClick={onSubClick}>
+          Most Reviewed
+        </button>
       </div>
       <div className="inputs">
-        <input onChange={(e) => setRestauraunt(e.target.value)} placeholder="Restaurant" />
-        <input onChange={(e) => setLocation(e.target.value)} placeholder="Location" />
+        <input
+          onChange={(e) => setRestauraunt(e.target.value)}
+          placeholder="Restaurant"
+        />
+        <input
+          onChange={(e) => setLocation(e.target.value)}
+          placeholder="Location"
+        />
       </div>
       <button className="buttonSearch">Let's Go</button>
     </div>
